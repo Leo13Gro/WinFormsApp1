9e62ceb8eaa712e4a805107d5b5396b3b143792b
Kuimov Timofey <111085197+Leo13Gro@users.noreply.github.com>
2024-09-09 22:50:43 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,83 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int space = 0;
+        int dashInsertionFlag = 0;
+
+        //private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    if (char.IsLetter(e.KeyChar) || e.KeyChar == '-')
+        //    {
+        //        textBox2.Text += e.KeyChar;
+        //    }
+        //}
+
+        //private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        //{
+        //    if (e.KeyCode == Keys.K)
+        //    {
+        //        this.Text = "Tab";
+        //        textBox1.SelectionStart = 0;
+        //    }
+        //}
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar) || (e.KeyChar == ' ') || (e.KeyChar == '-'))
+                if (textBox1.SelectionStart == 0)
+                    if (e.KeyChar == ' ' || e.KeyChar == '-') e.Handled = true;
+                    else e.KeyChar = Char.ToUpper(e.KeyChar);
+
+                else if (e.KeyChar == ' ')
+                    if (space == 0) space = 1;
+                    else e.Handled = true;
+                
+                else if (e.KeyChar == '-')
+                    if (dashInsertionFlag == 0)
+                        dashInsertionFlag = 1;
+                    else e.Handled = true;
+                
+                else if (dashInsertionFlag == 1)
+                {
+                    e.KeyChar = Char.ToUpper(e.KeyChar);
+                    dashInsertionFlag = -1;
+                }
+
+                else if (space > 0)
+                {
+                    e.KeyChar = Char.ToUpper(e.KeyChar); space++;
+                }
+                else { }
+            else e.Handled = true;
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (space == -1 && e.KeyCode == Keys.Enter)
+            {
+                textBox2.Text += textBox1.Text + Environment.NewLine;
+                textBox1.Text = string.Empty;
+                dashInsertionFlag = 0;
+                space = 0;
+            }
+            if (space == 2)
+            {
+                textBox1.AppendText(".");
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.SelectionLength = 0;
+            }
+            if (space == 3)
+            {
+                textBox1.AppendText(".");
+                textBox1.SelectionStart = 0;
+                textBox1.SelectionLength = textBox1.Text.Length;
+                space = -1;
+            };
+        }
+    }
+}
