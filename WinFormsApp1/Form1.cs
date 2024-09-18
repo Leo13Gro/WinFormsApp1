8fad723bf9a74c9f7ca43c8ab7293c1f2af459d3
Куимов Тимофей <timofey.kuimov@inside.glowbyte.com>
2024-09-18 20:23:39 +0300

    small changes

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -8,23 +8,7 @@ namespace WinFormsApp1
         }
         int space = 0;
         int dashInsertionFlag = 0;
-
-        //private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        //{
-        //    if (char.IsLetter(e.KeyChar) || e.KeyChar == '-')
-        //    {
-        //        textBox2.Text += e.KeyChar;
-        //    }
-        //}
-
-        //private void textBox1_KeyUp(object sender, KeyEventArgs e)
-        //{
-        //    if (e.KeyCode == Keys.K)
-        //    {
-        //        this.Text = "Tab";
-        //        textBox1.SelectionStart = 0;
-        //    }
-        //}
+        bool dot = true;
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -34,9 +18,11 @@ namespace WinFormsApp1
                     else e.KeyChar = Char.ToUpper(e.KeyChar);
 
                 else if (e.KeyChar == ' ')
+                {
+                    dashInsertionFlag = -1;
                     if (space == 0) space = 1;
                     else e.Handled = true;
-                
+                }
                 else if (e.KeyChar == '-')
                     if (dashInsertionFlag == 0)
                         dashInsertionFlag = 1;
@@ -62,20 +48,21 @@ namespace WinFormsApp1
             {
                 textBox2.Text += textBox1.Text + Environment.NewLine;
                 textBox1.Text = string.Empty;
-                dashInsertionFlag = 0;
                 space = 0;
             }
-            if (space == 2)
+            if (space == 2 && dot)
             {
                 textBox1.AppendText(".");
                 textBox1.SelectionStart = textBox1.Text.Length;
                 textBox1.SelectionLength = 0;
+                dot = false;
             }
             if (space == 3)
             {
                 textBox1.AppendText(".");
                 textBox1.SelectionStart = 0;
                 textBox1.SelectionLength = textBox1.Text.Length;
+                dashInsertionFlag = 0;
                 space = -1;
             };
         }
