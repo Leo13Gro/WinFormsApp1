82c5d83f7641197a74147c1fa4078b5ff51e7343
Kuimov Timofey <111085197+Leo13Gro@users.noreply.github.com>
2024-09-18 17:25:31 +0300

    change logic

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,5 +1,3 @@
-using System.Globalization;
-
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -8,9 +6,8 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
-        int space = 0;
-        int dashInsertionFlag = 0;
-        string template = "U+d U+.U+.";
+        Boolean dotFlag = false;
+        string template = ""; // После маленьких букв точка не ставится Пример: U1l U1.U1.
         int i = 0;
         int num = 0;
 
@@ -38,83 +35,103 @@ namespace WinFormsApp1
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {   
-            // if (template[i] == 'U'){
-            //     num = 0;
-            //     if (Char.IsLetter(e.KeyChar))
-            //         if (num < template[i+1])
-            //         {
-            //             e.KeyChar = Char.ToUpper(e.KeyChar);
-            //             num++;
-            //         }
-            //         else
-            //         {
-
-            //         }
-            //     else e.Handled = true;
-            //     if (template[i+1] == '+')
-            //         i++;
-            // }
-            // else if (template[i] == 'l'){
-
-            // }
-            // else if (template[i] == ' '){
-                
-            // }
-            // else if (template[i] == '.'){
-                
-            // }
-
-            if (Char.IsLetter(e.KeyChar) || (e.KeyChar == ' ') || (e.KeyChar == '-'))
-                if (textBox1.SelectionStart == 0)
-                    if (e.KeyChar == ' ' || e.KeyChar == '-') e.Handled = true;
-                    else e.KeyChar = Char.ToUpper(e.KeyChar);
-
-                else if (e.KeyChar == ' ')
-                    if (space == 0) space = 1;
-                    else e.Handled = true;
-                
-                else if (e.KeyChar == '-')
-                    if (dashInsertionFlag == 0)
-                        dashInsertionFlag = 1;
-                    else e.Handled = true;
-                
-                else if (dashInsertionFlag == 1)
+        {
+            if (template.Equals(""))
+            {
+                return;
+            }
+            if (i >= template.Length)
+            {
+                e.Handled = true;
+                return;
+            }
+            if (template[i] == 'U')
+            {
+                if (num < (int)Char.GetNumericValue(template[i + 1]))
                 {
-                    e.KeyChar = Char.ToUpper(e.KeyChar);
-                    dashInsertionFlag = -1;
+                    if (Char.IsLetter(e.KeyChar))
+                    {
+                        num++;
+                        e.KeyChar = Char.ToUpper(e.KeyChar);
+                        if (num == (int)Char.GetNumericValue(template[i + 1]))
+                        {
+                            if (i+2 < template.Length && template[i + 2] == '.')
+                            {
+                                dotFlag = true;
+                                i += 3;
+                            }
+                            else i += 2;
+                            num = 0;
+                        }
+                        return;
+                    }
+                    else e.Handled = true;
                 }
-
-                else if (space > 0)
+            }
+            if (template[i] == 'l' && Char.IsLetter(e.KeyChar))
+            {
+                if (i+1 < template.Length && e.KeyChar == template[i + 1])
                 {
-                    e.KeyChar = Char.ToUpper(e.KeyChar); space++;
+                    i += 2;
+                    return;
                 }
-                else { }
-            else e.Handled = true;
+                else return;
+            }
+            if (template[i] == ' ')
+            {
+                i++;
+                return;
+            }
+
+            e.Handled = true;
         }
 
         private void textBox1_KeyUp(object sender, KeyEventArgs e)
         {
-            if (space == -1 && e.KeyCode == Keys.Enter)
+            if (template.Equals("") && e.KeyCode == Keys.Enter)
             {
-                textBox2.Text += textBox1.Text + Environment.NewLine;
+                template = textBox1.Text;
                 textBox1.Text = string.Empty;
-                dashInsertionFlag = 0;
-                space = 0;
             }
-            if (space == 2)
+            if (dotFlag)
             {
                 textBox1.AppendText(".");
                 textBox1.SelectionStart = textBox1.Text.Length;
                 textBox1.SelectionLength = 0;
+                dotFlag = false;
             }
-            if (space == 3)
+            if (i+1 == template.Length && template[i] == 'l' && e.KeyCode == Keys.Enter)
             {
-                textBox1.AppendText(".");
-                textBox1.SelectionStart = 0;
-                textBox1.SelectionLength = textBox1.Text.Length;
-                space = -1;
-            };
+                textBox2.Text += textBox1.Text + Environment.NewLine;
+                textBox1.Text = string.Empty;
+                i = 0;
+            }
+            if (i >= template.Length && e.KeyCode == Keys.Enter)
+            {
+                textBox2.Text += textBox1.Text + Environment.NewLine;
+                textBox1.Text = string.Empty;
+                i = 0;
+            }
+            //if (space == -1 && e.KeyCode == Keys.Enter)
+            //{
+            //    textBox2.Text += textBox1.Text + Environment.NewLine;
+            //    textBox1.Text = string.Empty;
+            //    dashInsertionFlag = 0;
+            //    space = 0;
+            //}
+            //if (space == 2)
+            //{
+            //    textBox1.AppendText(".");
+            //    textBox1.SelectionStart = textBox1.Text.Length;
+            //    textBox1.SelectionLength = 0;
+            //}
+            //if (space == 3)
+            //{
+            //    textBox1.AppendText(".");
+            //    textBox1.SelectionStart = 0;
+            //    textBox1.SelectionLength = textBox1.Text.Length;
+            //    space = -1;
+            //};
         }
     }
 }
